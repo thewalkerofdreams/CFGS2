2dd3eed677606e93ac2acf57390be21d2a1cfe2f
Yeray Campanario Fernández <thewalkerofdreams@gmail.com>
2020-01-13 14:29:38 +0100

    commit

---
+++ Desarrollo de Interfaces/Animations_Space_DI/Animations_Space_DI_UI/Views/MainPage.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.System;
+using Windows.UI.Core;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// La plantilla de elemento Página en blanco está documentada en https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0xc0a
+
+namespace Animations_Space_DI_UI
+{
+    /// <summary>
+    /// Página vacía que se puede usar de forma independiente o a la que se puede navegar dentro de un objeto Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            // Set the input focus to ensure that keyboard events are raised.
+            this.Loaded += delegate { this.Focus(FocusState.Programmatic); };
+        }
+
+        private void MediaButton_Click(object sender, RoutedEventArgs e)
+        {
+            switch ((sender as Button).Name)
+            {
+                //case "PlayButton": DemoMovie.Play(); break;
+                //case "PauseButton": DemoMovie.Pause(); break;
+                //case "StopButton": DemoMovie.Stop(); break;
+            }
+        }
+
+        private static bool IsCtrlKeyPressed()
+        {
+            var ctrlState = CoreWindow.GetForCurrentThread().GetKeyState(VirtualKey.Control);
+            return (ctrlState & CoreVirtualKeyStates.Down) == CoreVirtualKeyStates.Down;
+        }
+
+        private void Grid_KeyDown(object sender, KeyRoutedEventArgs e)
+        {
+            if (IsCtrlKeyPressed())
+            {
+                switch (e.Key)
+                {
+                    //case VirtualKey.P: DemoMovie.Play(); break;
+                    //case VirtualKey.A: DemoMovie.Pause(); break;
+                    //case VirtualKey.S: DemoMovie.Stop(); break;
+                }
+            }
+        }
+    }
+}
