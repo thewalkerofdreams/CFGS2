ff9a73064141ec17abeda4aae11561b99f1f93e7
Yeray <thewalkerofdreams@gmail.com>
2020-01-17 19:48:50 +0100

    RaccoonCompany Mod

--- Sistemas de Gestión/RaccoonCompany/RaccoonCompany_UI/Js/IndexJS.js
+++ Sistemas de Gestión/RaccoonCompany/RaccoonCompany_UI/Js/IndexJS.js
@@ -26,107 +26,248 @@ window.onload = inicializa;
 
 function inicializa() {
     cargarListadoPersonasConDepartamento();
+    document.getElementById("btnAddPerson").addEventListener("click", insertarPersona, false);
 }
 
-/*function cargarListadoPersonas() {
+function cargarListadoPersonasConDepartamento(){
     var miLlamada = new XMLHttpRequest();
-    var miLlamada2 = new XMLHttpRequest();
-    miLlamada.open("GET", "https://crudpersonasui-victor.azurewebsites.net/api/personasapi");
-    miLlamada2.open("GET", "https://crudpersonasui-victor.azurewebsites.net/api/departamentosapi");
+    miLlamada.open("GET", "https://crudpersonasui-victor.azurewebsites.net/api/departamentosapi");
 
     //Definicion estados
     miLlamada.onreadystatechange = function () {
 
-        
-            if (miLlamada.readyState == 4 && miLlamada.status == 200 && miLlamada2.readyState == 4 && miLlamada2.status == 200) {
-                var arrayPersonas = JSON.parse(miLlamada.responseText);
-                //var persona = arrayPersonas[0]; 
-                //document.getElementById("divApellidos").innerHTML = persona.apellidos;
+        if (miLlamada.readyState == 4 && miLlamada.status == 200) {
+            var arrayDepartamentos = JSON.parse(miLlamada.responseText);//Obtenemos la lista de departamentos
+            cargarListadoPersonas(arrayDepartamentos);//Cargamos la lista de personas
+        }
+    };
 
-                for (i = 0; i < arrayPersonas.length; i++) {
-                    var td = document.createElement('td');//Creamos un tag de <td>
-                    td.innerHTML = "" + arrayPersonas[i].nombre + "";
-                    document.getElementById("EmployeeFirstName").appendChild(td);//Agregamos el tag <td> a la lista (<tr>)
-                    td.innerHTML = "" + arrayPersonas[i].apellidos + "";
-                    document.getElementById("EmployeeLastName").appendChild(td);//Agregamos el tag <td> a la lista (<tr>)
-                    td.innerHTML = "" + arrayPersonas[i].fechaNacimiento + "";
-                    document.getElementById("EmployeeBirthday").appendChild(td);//Agregamos el tag <td> a la lista (<tr>)
-                    var arrayDepartamentos = JSON.parse(miLlamada2.responseText);
-                    Boolean nombreEncontrado = false;
-                    for (i = 0; i < arrayDepartamentos.length && !nombreEncontrado; i++) {
-                        if (arrayDepartamentos[i].id == arrayPersonas[i].idDepartamento) {
-                            td.innerHTML = "" + arrayDepartamentos[i].nombre + "";
-                            nombreEncontrado = true;
-                        }
+        miLlamada.send();
+}
+
+function reloadTable() {//Actualizamos la tabla
+    var table = document.getElementById("tableEmployee");
+    var rowCount = table.rows.length;//Obtenemos el número de filas de la tabla
+    for (var i = 1; i < rowCount; i++) {//Eliminamos todas las filas de la tabla
+        table.deleteRow(i);
+    }
+    cargarListadoPersonasConDepartamento();//Volvemos a cargar el listado de personas
+}
+
+//Hace una peticion get de una persona segun id (GET{ID})
+function consultarPersona(id) {
+
+    var persona;
+    var miLlamada = new XMLHttpRequest();
+    miLlamada.open("GET", "https://crudpersonasui-victor.azurewebsites.net/api/PersonasAPI/" + id, false);
+
+    miLlamada.onreadystatechange = function () {
+        if (miLlamada.readyState == 4 && miLlamada.status == 200) {
+            persona = JSON.parse(miLlamada.responseText);
+        }
+    }
+
+    miLlamada.send();
+
+    return persona;
+}
+
+function clickEditar() {//Lo utilizaremos para actualizar un empleado
+
+    var edit = document.getElementById("editEmployeeModal");
+    var save = document.getElementById("inputSaveEmployee");
+    var cancel = document.getElementById("inputCancelSaveEmployee");
+    edit.style.display = "block";
+
+    var pers = consultarPersona(this.id);
+
+    //var id = document.getElementById("idPersona");
+    //id.setAttribute("value", pers.idPersona);
+    document.getElementById("nombreEdit").setAttribute("value", pers.nombre);
+    document.getElementById("apellidosEdit").setAttribute("value", pers.apellidos);
+    //document.getElementById("telefonoE").setAttribute("value", pers.telefono);
+    var porque2 = pers.fechaNacimiento.substr(0, 4) + "-" + pers.fechaNacimiento.substr(5, 2) + "-" + pers.fechaNacimiento.substr(8, 2);
+    document.getElementById("fechaNacimientoEdit").setAttribute("value", porque2);
+    document.getElementById("departamentoEdit").setAttribute("value", pers.idDepartamento);
+
+    save.onclick = function () {
+        //id.focus();
+
+        var empleado = new Object();
+        empleado.idPersona = this.id;
+        empleado.nombre = document.getElementById("nombreEdit").value;
+        empleado.apellidos = document.getElementById("apellidosEdit").value;
+        empleado.telefono = "000000000";
+        empleado.fechaNacimiento = document.getElementById("fechaNacimientoEdit").value;
+        empleado.idDepartamento = document.getElementById("departamentoEdit").value;
+
+        var miLlamada = new XMLHttpRequest();
+        miLlamada.open("PUT", "https://crudpersonasui-victor.azurewebsites.net/api/PersonasAPI/" + this.id);
+        miLlamada.setRequestHeader('Content-type', 'application/json');
+
+        if (miLlamada.readyState == 4 && miLlamada.status == 200) {
+            //actualizar
+            alert("Employee edited!");
+            edit.style.display = "none";
+            reloadTable();
+        }
+
+        miLlamada.send(JSON.stringify(empleado));
+    }
+
+    cancel.onclick = function () {
+        edit.style.display = "none";
+    }
+}
+
+//Eliminar una persona (DELETE)
+function clickEliminar() {
+
+    //modal
+    var id = this.id;
+    var modal = document.getElementById("deleteEmployeeModal");
+    var acept = document.getElementById("inputAceptDelete");
+    var cancel = document.getElementById("inputCancelDelete");
+    modal.style.display = "block";
+
+    //Si acepta
+    acept.onclick = function () {
+        modal.style.display = "none";
+
+        //eliminar
+        var llamadaEliminar = new XMLHttpRequest();
+        llamadaEliminar.open("DELETE", "https://crudpersonasui-victor.azurewebsites.net/api/PersonasAPI/" + id);
+        llamadaEliminar.onreadystatechange = function () {
+
+            if (llamadaEliminar.readyState == 4 && llamadaEliminar.status == 204) {
+
+                reloadTable();//Actualizamos la tabla
+                alert("Employee deleted!");
+            }
+        }
+        llamadaEliminar.send();
+    }
+
+    //Si cancela
+    cancel.onclick = function () {
+        modal.style.display = "none";
+    }
+}
+
+function cargarListadoPersonas(arrayDepartamentos) {
+    var miLlamada = new XMLHttpRequest();
+    miLlamada.open("GET", "https://crudpersonasui-victor.azurewebsites.net/api/personasapi");
+
+    //Definicion estados
+    miLlamada.onreadystatechange = function () {
+           
+        if (miLlamada.readyState == 4 && miLlamada.status == 200) {
+            var table = document.getElementById("tBodyEmployee");
+            var arrayPersonas = JSON.parse(miLlamada.responseText);
+
+            for (i = 0; i < arrayPersonas.length; i++) {
+                var tr = document.createElement('tr');
+                document.getElementById("tBodyEmployee").appendChild(tr);
+
+                var td = document.createElement('td');//Creamos un tag <td> para el nombre del empleado
+                td.innerHTML = "" + arrayPersonas[i].nombre + "";
+                tr.appendChild(td);
+
+                var td2 = document.createElement('td');//Creamos un tag <td> para el apellido del empleado
+                td2.innerHTML = "" + arrayPersonas[i].apellidos + "";
+                tr.appendChild(td2);
+
+                var td3 = document.createElement('td');//Creamos un tag <td> para la fecha de nacimiento del empleado
+                td3.innerHTML = "" + arrayPersonas[i].fechaNacimiento + "";
+                tr.appendChild(td3);
+
+                var td4 = document.createElement('td');//Creamos un tag <td> para el departamento del empleado
+                var nombreEncontrado = false;
+                for (j = 0; i < arrayDepartamentos.length && !nombreEncontrado; j++) {
+                    if (arrayDepartamentos[j].id == arrayPersonas[i].idDepartamento) {
+                        td4.innerHTML = "" + arrayDepartamentos[j].nombre + "";
+                        nombreEncontrado = true;
                     }
-                    document.getElementById("EmployeeDepartament").appendChild(td);//Agregamos el tag <td> a la lista (<tr>)
-                }
-            
+                }    
+                tr.appendChild(td4); 
 
+                var tdButtons = document.createElement("td");//Agregamos un tag <td> para los botones
+                    
+                var edit = document.createElement("input");
+                edit.setAttribute("type", "image");
+                edit.setAttribute("id", arrayPersonas[i].idPersona);
+                edit.setAttribute("src", "../Resources/Images/icon_edit.png");
+                edit.setAttribute("width", "30");
+                edit.setAttribute("heigth", "30");
+                edit.addEventListener("click", clickEditar, false);
+                    
+                var remove = document.createElement("input");
+                remove.setAttribute("type", "image");
+                remove.setAttribute("id", arrayPersonas[i].idPersona);
+                remove.setAttribute("src", "../Resources/Images/icon_delete.png");
+                remove.setAttribute("width", "30");
+                remove.setAttribute("heigth", "30");
+                remove.addEventListener("click", clickEliminar, false);
+
+                tdButtons.appendChild(edit);//Agregamos los botones al tag <td>
+                tdButtons.appendChild(remove);
+
+                tr.appendChild(tdButtons);//Le asignamos el tag <td> al tag <tr> 
+
+                table.appendChild(tr);//Le asignamos el tag <tr> a la tabla
+            } 
+        }
     };
 
     miLlamada.send();
-    miLlamada2.send();
-    }*/
+}
 
-    function cargarListadoPersonasConDepartamento(){
-        var miLlamada = new XMLHttpRequest();
-        miLlamada.open("GET", "https://crudpersonasui-victor.azurewebsites.net/api/departamentosapi");
+function insertarPersona() {
 
-        //Definicion estados
-        miLlamada.onreadystatechange = function () {
+    var createModal = document.getElementById("addEmployeeModal");
+    var btnCreate = document.getElementById("btnAddPerson");
+    var btnCancel = document.getElementById("btnCancelPerson");
+    createModal.style.display = "block";
 
-            if (miLlamada.readyState == 4 && miLlamada.status == 200) {
-                var arrayDepartamentos = JSON.parse(miLlamada.responseText);//Obtenemos la lista de departamentos
-                cargarListadoPersonas(arrayDepartamentos);//Cargamos la lista de personas
-            }
-        };
+    btnCreate.onclick = function () {//Si pulsamos crear...
+        var id = 1;
+        var nombre = document.getElementById("nombre").value;
+        var apellidos = document.getElementById("apellidos").value;
+        var fechaNacimiento = document.getElementById("fechaNacimiento").value;
+        //var telefono = document.getElementById("telefono").value;
+        var idDepartamento = document.getElementById("departamento").value;
 
-        miLlamada.send();
+        var empleado = new Object();
+        empleado.idPersona = id;
+        empleado.nombre = nombre;
+        empleado.apellidos = apellidos;
+        empleado.fechaNacimiento = fechaNacimiento;
+        empleado.telefono = "000000000";
+        empleado.idDepartamento = idDepartamento;
+
+        insertarPersonaPost(empleado);
     }
 
-    function cargarListadoPersonas(arrayDepartamentos){
-        var miLlamada = new XMLHttpRequest();
-        miLlamada.open("GET", "https://crudpersonasui-victor.azurewebsites.net/api/personasapi");
-
-        //Definicion estados
-        miLlamada.onreadystatechange = function () {
-           
-            if (miLlamada.readyState == 4 && miLlamada.status == 200) {
-                var arrayPersonas = JSON.parse(miLlamada.responseText);
-
-                for (i = 0; i < arrayPersonas.length; i++) {
-                    var tr = document.createElement('tr');
-                    tr.id = "nuevoTr";
-                    document.getElementById("tBodyEmployee").appendChild(tr);
-                     var td = document.createElement('td');//Creamos un tag <td>
-                    var td2 = document.createElement('td');//Creamos un tag <td>
-                    var td3 = document.createElement('td');//Creamos un tag <td>
-                    var td4 = document.createElement('td');//Creamos un tag <td>
-                    for (i = 0;  i < arrayPersonas.length; i++) {
-                         td.innerHTML = "" + arrayPersonas[i].nombre + "";
-                        //document.getElementById("EmployeeFirstName").appendChild(td);//Introducimos el nombre del empleado en la lista
-                        document.getElementById("nuevoTr").appendChild(td);
-                        td2.innerHTML = "" + arrayPersonas[i].apellidos + "";
-                        //document.getElementById("EmployeeLastName").appendChild(td2);//Introducimos el apellido del empleado en la lista
-                        document.getElementById("nuevoTr").appendChild(td2);
-                        td3.innerHTML = "" + arrayPersonas[i].fechaNacimiento + "";
-                        //document.getElementById("EmployeeBirthday").appendChild(td3);//Introducimos la fecha de nacimiento del empleado en la lista
-                        document.getElementById("nuevoTr").appendChild(td3);
-                        var nombreEncontrado = false;
-                        for (j = 0; i < arrayDepartamentos.length && !nombreEncontrado; j++) {
-                            if (arrayDepartamentos[j].id == arrayPersonas[i].idDepartamento) {
-                                td4.innerHTML = "" + arrayDepartamentos[j].nombre + "";
-                                nombreEncontrado = true;
-                            }
-                        }
-                        //document.getElementById("EmployeeDepartament").appendChild(td4);//Agregamos el tag <td> a la lista (<tr>)
-                        document.getElementById("nuevoTr").appendChild(td4);
-                       
-                    }
-                } 
-            }
-        };
-
-        miLlamada.send();
+    btnCancel.onclick = function () {//Si pulsamos cancelar...
+        createModal.style.display = "none";
     }
+
+    function insertarPersonaPost(empleado) {//Insertamos el empleado por post
+
+        var llamadaInsertar = new XMLHttpRequest();
+        llamadaInsertar.open('POST', "https://crudpersonasui-victor.azurewebsites.net/api/PersonasAPI/", false);
+        llamadaInsertar.setRequestHeader('Content-type', 'application/json');
+
+        llamadaInsertar.onreadystatechange = function () {
+            if (llamadaInsertar.readyState == 4 && llamadaInsertar.status == 200) {
+                alert("Employee inserted!");
+                crear.style.display = "none";
+                reloadTable();
+            }
+        }
+
+        llamadaInsertar.send(JSON.stringify(empleado));
+    }
+
+    
+}
