3f71df73712f6cc345d6b5aa290a212c13f284bc
Yeray Campanario <thewalkerofdreams@gmail.com>
2020-02-09 17:40:10 +0100

    estructura examen

---
+++ Estructura_Examen_DI/Examen_DI/Examen_DI_DAL/Connections/ClsMyConnection.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Examen_DI_DAL.Connections
+{
+    public class ClsMyConnection
+    {
+        public static string getUriBase()
+        {
+            String uriBase = "https://crudtoflamaapi.azurewebsites.net/api";
+            return uriBase;
+        }
+    }
+}
