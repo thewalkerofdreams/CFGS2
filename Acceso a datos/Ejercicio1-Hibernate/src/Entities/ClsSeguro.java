c55a45212eafbbed0553e6e5fe5be3e4ed64b10f
Yeray Campanario Fernández <thewalkerofdreams@gmail.com>
2020-03-03 12:46:47 +0100

    ACDA

---
+++ Acceso a datos/Ejercicio1-Hibernate/src/Entities/ClsSeguro.java
@@ -0,0 +1,93 @@
+package Entities;
+
+import java.util.Date;
+
+public class ClsSeguro {
+
+    private int idSeguro;
+    private String nif;
+    private String nombre;
+    private String apellido1;
+    private String apellido2;
+    private int edad;
+    private int numHijos;
+    private Date fechaCreacion;
+
+    public ClsSeguro(){
+    }
+
+    public ClsSeguro(int idSeguro, String nif, String nombre, String apellido1, String apellido2, int edad, int numHijos, Date fechaCreacion){
+        this.idSeguro = idSeguro;
+        this.nif = nif;
+        this.nombre = nombre;
+        this.apellido1 = apellido1;
+        this.apellido2 = apellido2;
+        this.edad = edad;
+        this.numHijos = numHijos;
+        this.fechaCreacion = fechaCreacion;
+    }
+
+    public int getIdSeguro() {
+        return idSeguro;
+    }
+
+    public void setIdSeguro(int idSeguro) {
+        this.idSeguro = idSeguro;
+    }
+
+    public String getNif() {
+        return nif;
+    }
+
+    public void setNif(String nif) {
+        this.nif = nif;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido1() {
+        return apellido1;
+    }
+
+    public void setApellido1(String apellido1) {
+        this.apellido1 = apellido1;
+    }
+
+    public String getApellido2() {
+        return apellido2;
+    }
+
+    public void setApellido2(String apellido2) {
+        this.apellido2 = apellido2;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public int getNumHijos() {
+        return numHijos;
+    }
+
+    public void setNumHijos(int numHijos) {
+        this.numHijos = numHijos;
+    }
+
+    public Date getFechaCreacion() {
+        return fechaCreacion;
+    }
+
+    public void setFechaCreacion(Date fechaCreacion) {
+        this.fechaCreacion = fechaCreacion;
+    }
+}
