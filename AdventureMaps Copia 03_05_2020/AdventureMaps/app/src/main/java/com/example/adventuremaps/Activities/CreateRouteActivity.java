f9916a10f5d6e460089722864d2686c02d41064c
Yeray Campanario <thewalkerofdreams@gmail.com>
2020-05-03 10:16:22 +0200

    AdventureMaps Security Copy

---
+++ AdventureMaps Copia 03_05_2020/AdventureMaps/app/src/main/java/com/example/adventuremaps/Activities/CreateRouteActivity.java
@@ -0,0 +1,161 @@
+package com.example.adventuremaps.Activities;
+
+import android.app.AlertDialog;
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.fragment.app.FragmentManager;
+import androidx.lifecycle.ViewModelProviders;
+
+import com.example.adventuremaps.FireBaseEntities.ClsRoute;
+import com.example.adventuremaps.FireBaseEntities.ClsRoutePoint;
+import com.example.adventuremaps.Fragments.GoogleMapsFragment;
+import com.example.adventuremaps.R;
+import com.example.adventuremaps.ViewModels.RouteActivitiesVM;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class CreateRouteActivity extends AppCompatActivity implements ActivityCompat.OnRequestPermissionsResultCallback {
+
+    private RouteActivitiesVM viewModel;
+    private DatabaseReference routeReference = FirebaseDatabase.getInstance().getReference("ClsRoute");
+    private ProgressDialog progressDialog;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_route);
+
+        //Instanciamos el VM
+        viewModel = ViewModelProviders.of(this).get(RouteActivitiesVM.class);
+        viewModel.set_actualEmailUser(getIntent().getStringExtra("ActualEmail"));
+
+        //Instanciamos los elementos de la UI
+        progressDialog = new ProgressDialog(this);
+        progressDialog.setMessage("Saving new route");
+    }
+
+    /**
+     * Interfaz
+     * Nombre: intentarMarcarLocalizacion
+     * Comentario: Este método nos permite marcar la última lozalización pulsada en el mapa.
+     * Si no existe una localización marcada anteriormente, no se creará ningún marcador.
+     * Cabecera: public boolean intentarMarcarLocalizacion(View view)
+     * Entrada:
+     *   -View view
+     * Postcondiciones: El método marca la última localización pulsada en el mapa, si existe
+     * alguna.
+     * */
+    public void intentarMarcarLocalizacion(View view){
+        if(viewModel.get_lastLocalizationClicked() != null){//Si se ha marcado una localización
+            FragmentManager fm = getSupportFragmentManager();
+            GoogleMapsFragment fragment = (GoogleMapsFragment)fm.findFragmentById(R.id.fragmentGoogleMapsCreateRouteActivity);
+            LatLng latLng = new LatLng(viewModel.get_lastLocalizationClicked().getLatitude(), viewModel.get_lastLocalizationClicked().getLongitude());
+            //Marcamos esa posición
+            fragment.colocarMarcador(latLng);
+        }
+    }
+
+    /**
+     * Interfaz
+     * Nombre: trySaveRouteDialog
+     * Comentario: Este método nos permite guardar la ruta actual rellenando un dialogo con los
+     * datos finales de la ruta, si la ruta actual tiene menos de dos puntos de localizaciones, no
+     * se lanzará el dialogo, sino que se lanzará un mensaje de error informando al usuario que debe
+     * marcar como mínimo dos puntos en el mapa.
+     * Cabecera: public void trySaveRouteDialog(View v)
+     * Entrada:
+     *  -View v
+     * Postcondiciones: El método almacena la ruta actual en la plataforma de FireBase o devuelve un mensaje
+     * de error por pantalla si la ruta contiene menos de dos puntos de localización marcados en el mapa.
+     */
+    public void trySaveRouteDialog(View v){
+        if(viewModel.get_localizationPoints().size() >= 2){//Si la ruta actual tiene como mínimo dos puntos de localización
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            final EditText nameEdit = new EditText(this);
+            nameEdit.setHint(getString(R.string.hint_route_name));//Le insertamos una pista
+            builder.setTitle(getString(R.string.save_route))
+                    .setView(nameEdit)
+                    .setMessage(getString(R.string.hint_route_name))
+                    .setPositiveButton(getString(R.string.save), new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            String routeName = nameEdit.getText().toString();//Nombre de la ruta a crear
+
+                            if (routeName.isEmpty()) {//Si el nombre está vacío
+                                Toast.makeText(getApplication(), getApplication().getString(R.string.route_name_empty), Toast.LENGTH_SHORT).show();
+                            } else {
+                                saveRoute(routeName);//Almacenamos la nueva ruta en la plataforma Firebase
+                            }
+                        }
+                    })
+                    .setNegativeButton(getString(R.string.cancel), new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            dialog.cancel();
+                        }
+                    });
+            AlertDialog alertDialogCreateGameMode = builder.create();
+            alertDialogCreateGameMode.show();//Lanzamos el dialogo
+        }else{
+            Toast.makeText(getApplication(), getApplication().getString(R.string.insufficient_markers), Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    /**
+     * Interfaz
+     * Nombre: saveRoute
+     * Comentario: El método almacena la ruta actual junto con sus puntos en la plataforma Firebase.
+     * Cabecera: private void saveRoute(String routeName)
+     * Entrada:
+     *  -String routeName
+     * Postcondiciones: El método almacena la ruta actual con un nombre determinado en la plataforma de Firebase.
+     */
+    private void saveRoute(String routeName){
+        progressDialog.show();//Mostramos el progressDialog mientras se guarde la nueva ruta
+        final String routeId = routeReference.push().getKey();//Obtenemos una id para la ruta
+
+        //Almacenamos la ruta
+        final ClsRoute newRoute = new ClsRoute(routeId, routeName, false, System.currentTimeMillis(), FirebaseAuth.getInstance().getCurrentUser().getEmail());
+        FirebaseDatabase.getInstance().getReference("Users").
+                child(FirebaseAuth.getInstance().getCurrentUser().getUid()).child("routes").child(newRoute.getRouteId())
+                .setValue(newRoute).addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {//Una vez almacenada la ruta, almacenamos los puntos que la forman
+                //Almacenamos los puntos de la ruta
+                for(int i = 0; i < viewModel.get_localizationPoints().size(); i++){
+                    String routePointId = routeReference.push().getKey();//Obtenemos una id para el punto de la ruta
+                    ClsRoutePoint newRoutePoint = new ClsRoutePoint(routePointId,(long) viewModel.get_localizationPoints().get(i).getPriority(),
+                            routeId, viewModel.get_localizationPoints().get(i).getMarker().getPosition().latitude, viewModel.get_localizationPoints().get(i).getMarker().getPosition().longitude);
+
+                    FirebaseDatabase.getInstance().getReference("Users").
+                            child(FirebaseAuth.getInstance().getCurrentUser().getUid()).child("routes").child(newRoute.getRouteId())
+                            .child("routePoints").child(newRoutePoint.getRoutePointId())
+                            .setValue(newRoutePoint).addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+                            viewModel.set_routePointsInserted(viewModel.get_routePointsInserted() + 1);//Aumentamos en uno el número de puntos insertados
+                            if(viewModel.get_localizationPoints().size() == viewModel.get_routePointsInserted()){//Si era el último punto de la ruta
+                                viewModel.set_routePointsInserted(0);//Volvemos a poner el contador a 0
+                                progressDialog.dismiss();//Ocultamos el progressDialog
+                                Toast.makeText(getApplication(), getApplication().getString(R.string.route_saved), Toast.LENGTH_SHORT).show();//Indicamos que se ha guardado la ruta
+                                finish();//Finalizamos la actividad actual
+                            }
+                        }
+                    });
+                }
+            }
+        });
+    }
+}
---
+++ AdventureMaps Copia 03_05_2020/AdventureMaps/app/src/main/java/com/example/adventuremaps/Fragments/Tutorial/CommonFragmentSection01.java
@@ -0,0 +1,122 @@
+package com.example.adventuremaps.Fragments.Tutorial;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.lifecycle.ViewModelProviders;
+
+import com.example.adventuremaps.R;
+import com.example.adventuremaps.ViewModels.TutorialViewPagerActivityVM;
+
+public class CommonFragmentSection01 extends Fragment {//Contendrá las páginas de la sección del mapa de inicio
+
+    private Button btnLastFragment, btnNextFragment;
+    private TutorialViewPagerActivityVM viewModel;
+
+    public CommonFragmentSection01() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        //Inflamos el layout del fragment
+        View rootView = inflater.inflate(R.layout.fragment_common, container, false);
+
+        //Instanciamos el VM
+        viewModel = ViewModelProviders.of(this).get(TutorialViewPagerActivityVM.class);
+
+        //Instanciamos los elementos de la UI
+        btnLastFragment = rootView.findViewById(R.id.last_button);
+        btnLastFragment.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                backToLasFragment();
+            }
+        });
+
+        btnNextFragment = rootView.findViewById(R.id.next_button);
+        btnNextFragment.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                enterNextFragment();
+            }
+        });
+
+        //Instanciamos el fragmento de inicio de la sección
+        loadActualPage();
+
+        return rootView;
+    }
+
+    /**
+     * Interfaz
+     * Nombre: enterNextFragment
+     * Comentario: Este método nos permite movernos al siguiente fragmento de la sección. Reemplazando
+     * el actual por el nuevo.
+     * Cabecera: private void enterNextFragment()
+     * Precondiciones:
+     *  -el usuario se debe encontrar en la primera página de la sección
+     * Postcondiciones: El método reemplaza el fragmento actual por el siguiente a este.
+     */
+    private void enterNextFragment() {
+        FragmentTransaction transaction = getChildFragmentManager().beginTransaction();
+        transaction.addToBackStack(null);
+
+        btnLastFragment.setVisibility(View.VISIBLE);//Volvemos visible el botón de back por si se encontraba invisible
+
+        transaction.replace(R.id.fragment_mainLayout, new StartSection02Fragment()).commit();//Remplazamos el fragmento
+        btnNextFragment.setVisibility(View.INVISIBLE);//Como no quedan más fragmentos posteriores lo ocultamos
+
+        viewModel.set_actualSubPageSection1(viewModel.get_actualSubPageSection1()+1);//Indicamos hacia que fragmento nos hemos movido
+    }
+
+    /**
+     * Interfaz
+     * Nombre: backToLasFragment
+     * Comentario: Este método nos permite movernos al anterior fragmento de la sección. Reemplazando
+     * el actual por el anterior.
+     * Cabecera: private void backToLasFragment()
+     * Precondiciones:
+     *  -el usuario se debe encontrar en la última página de la sección.
+     * Postcondiciones: El método reemplaza el fragmento actual por el anterior a este.
+     */
+    private void backToLasFragment(){
+        FragmentTransaction transaction = getChildFragmentManager().beginTransaction();
+        transaction.addToBackStack(null);
+
+        btnNextFragment.setVisibility(View.VISIBLE);//Volvemos visible el botón de next por si se encontraba invisible
+
+        transaction.replace(R.id.fragment_mainLayout, new StartSection01Fragment()).commit();//Remplazamos el fragmento
+        btnLastFragment.setVisibility(View.INVISIBLE);//Como no quedan más fragmentos anteriores lo ocultamos
+
+        viewModel.set_actualSubPageSection1(viewModel.get_actualSubPageSection1()-1);//Indicamos hacia que fragmento nos hemos movido
+    }
+
+    /**
+     * Interfaz
+     * Nombre: loadActualPage
+     * Comentario: Este método nos permite cargar una página de la sección,
+     * según el valor que tenga actualmente el atributo _actualSubPageSection1 del VM.
+     * Cabecera: private void loadActualPage()
+     * Postcondiciones: El método carga una página de la sección.
+     */
+    private void loadActualPage(){
+        FragmentTransaction transaction = getChildFragmentManager().beginTransaction();
+        transaction.addToBackStack(null);
+        switch (viewModel.get_actualSubPageSection1()){
+            case 0:
+                transaction.replace(R.id.fragment_mainLayout, new StartSection01Fragment()).commit();
+                break;
+            case 1:
+                transaction.replace(R.id.fragment_mainLayout, new StartSection02Fragment()).commit();
+                btnLastFragment.setVisibility(View.VISIBLE);//Volvemos visible el botón de retorno
+                btnNextFragment.setVisibility(View.INVISIBLE);//Volvemos invisible el botón next
+                break;
+        }
+    }
+}
