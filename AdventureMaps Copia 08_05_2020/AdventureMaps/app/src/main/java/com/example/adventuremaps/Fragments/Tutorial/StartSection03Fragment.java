093f505d6cda1c029c0d66560b7b23f1e083eb34
Yeray Campanario <thewalkerofdreams@gmail.com>
2020-05-08 13:52:56 +0200

    AdventureMaps Security Copy

---
+++ AdventureMaps Copia 08_05_2020/AdventureMaps/app/src/main/java/com/example/adventuremaps/Fragments/Tutorial/StartSection03Fragment.java
@@ -0,0 +1,25 @@
+package com.example.adventuremaps.Fragments.Tutorial;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+import com.example.adventuremaps.R;
+
+public class StartSection03Fragment extends Fragment {
+
+    public StartSection03Fragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        //Inflamos el layout del fragmento
+        View rootView = inflater.inflate(R.layout.fragment_start_section_03, container, false);
+
+        return rootView;
+    }
+}
---
+++ AdventureMaps Copia 08_05_2020/AdventureMaps/app/src/main/java/com/example/adventuremaps/Management/ApplicationConstants.java
@@ -0,0 +1,28 @@
+package com.example.adventuremaps.Management;
+
+public class ApplicationConstants {
+    //Permissions
+    public final static int REQUEST_CODE_PERMISSIONS_MAIN_TABBET_ACTIVITY_WITH_OFFLINE_MAPS = 1;
+    public final static int REQUEST_CODE_PERMISSIONS_MAIN_TABBET_ACTIVITY_WITH_START_MAP = 2;
+    public final static int REQUEST_CODE_PERMISSIONS_READ_EXTERNAL_STORAGE = 3;
+    public final static int REQUEST_CODE_START_ACTIVITY_FOR_RESULT_IMAGE_GALLERY = 4;
+    public final static int REQUEST_CODE_UPLOAD_IMAGE_FROM_OWN_GALLERY = 5;
+    public final static int REQUEST_CODE_PERMISSIONS_FINE_AND_COARSE_LOCATION = 6;
+    public final static int REQUEST_CODE_EDIT_LOCALIZATION_POINT = 7;
+    public final static int REQUEST_CODE_DETAILS_LOCALIZATION_POINT = 8;
+    public final static int REQUEST_CODE_CREATE_LOCALIZATION_POINT = 9;
+
+    //Icons Size
+    public final static int MARKER_WITH_SIZE = 120;
+    public final static int MARKER_HEIGHT_SIZE = 120;
+
+    //Default Coordenates
+    public final static double RLYEH_LATITUDE = -47.15;//Easter egg
+    public final static double RLYEH_LONGITUDE = -126.71666666666667;
+
+    //Maps Settings
+    public final static double MIN_ZOOM_LEVEL_OFFLINE_MAP = 2;
+
+    public final static String DEFAULT_MARKER_ICON_OFFLINE_MAPS = "DEFAULT_ICON";
+    public final static String MARKER_SELECTED_ICON_OFFLINE_MAPS = "MARKER_SELECTED";
+}
